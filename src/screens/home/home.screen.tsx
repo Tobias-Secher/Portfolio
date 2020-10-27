93cd8b4830d3c1c16ee82c2093c63d3159561a0e
Tobias Secher <tobias.secher@gmail.com>
2020-10-27 18:44:14 +0100

    Added intro & image to home

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -3,8 +3,22 @@ import './home.style.scss';
 
 const Home = (): JSX.Element => {
     return (
-        <div>
-            Home
+        <div className={'home-wrapper'}>
+            <div className={'home-wrapper__info'}>
+                <h1>Hi, I'm Tobias</h1>
+                <p>
+                    I’m a front-end developer specilizing in React and Angular.
+                    Check out my work and skills below and feel free to get in touch if you want to work with me
+                </p>
+                <button>GET IN TOUCH</button>
+            </div>
+            <div className={'home-wrapper__image'}>
+            </div>
+            <div className={'home-wrapper__project'}>pro</div>
+            <div className={'home-wrapper__skills'}>
+                <h4>Skills</h4>
+                
+            </div>
         </div>
     )
 }
