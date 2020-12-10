f38c3de34c955c1aa2159818412bb25969dd584a
tobiassecher <tobias.secher@gmail.com>
2020-12-10 14:18:59 +0100

    Removed files for old design

--- src/App.tsx
+++ src/App.tsx
@@ -3,63 +3,13 @@ import './App.scss';
 import {
   BrowserRouter as Router, Route, Switch,
 } from "react-router-dom";
-import Logo from './components/logo/logo.component';
-import Menu from './components/menu/menu.component';
-import { MenuModel } from './models/menu.model';
-import Home from './screens/home/home.screen';
-import Work from './screens/work/work.screen';
 
 const App = () => {
 
-  const menu: MenuModel[] = [
-    {
-      icon: ['fas', 'home'],
-      label: 'Home',
-      route: '/home',
-      screen: <Home />,
-      exact: true,
-      color: "blue"
-    },
-    {
-      icon: ['fas', 'code-branch'],
-      label: 'Work',
-      route: '/work',
-      screen: <Work />,
-      exact: true,
-      color: 'green'
-    },
-  ];
-
-  const routes = menu.map((m, index) => {
-    return (
-      <Route key={m.label} path={m.route}>
-        {m.screen}
-      </Route>
-    )
-  })
-
   return (
     <Router>
-      <div className={'wrapper'}>
-        <header className={'wrapper__header'}>
-          <h1>
-            <Logo />
-          Tobias <span>Secher</span>
-          </h1>
-        </header>
-        <div className={'wrapper__content wc'}>
-          <nav className={'wc__menu'}>
-            <Menu menu={menu} />
-          </nav>
-          <main className={'wc__content'}>
-            <Switch>
-              {routes}
-              <Route exact={true} path={'/'}>
-                <Home />
-              </Route>
-            </Switch>
-          </main>
-        </div>
+      <div>
+        asd
       </div>
     </Router>
   );
